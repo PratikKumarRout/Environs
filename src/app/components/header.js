a94ea22c8326274f62e00cac03060edb3ffff52e
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-21 19:56:18 +0530

    what is next update

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,12 +1,15 @@
+'use client'
+
+import { useState } from "react";
 import styles from "./styles/header.module.css";
 
 export default function Header({ location }) {
+
     return <section className={styles.header}>
-        <p id={styles.title}>
-            {location} </p>
-        <div className={styles.searchSection}>
-            <p id={styles.location}> Balasore</p> 
-            <p>🔍</p>
+       <div className={styles.searchSection}> 
+        <p id={styles.title}> {location} </p>
+            <p id={styles.location}>Balasore</p>
+            <button id={styles.searchButton}>🔍</button>
         </div>
     </section>;
 }
