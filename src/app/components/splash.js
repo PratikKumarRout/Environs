b0dd6ec79bf68f5b5e316a5791cdaba731d3792c
Pratik Kumar Rout <routpratikiven@gmail.com>
2026-01-24 16:17:35 +0530

    pro splash add

--- src/app/components/splash.js
+++ src/app/components/splash.js
@@ -1,10 +1,28 @@
 import styles from "./styles/splash.module.css";
+import { useEffect, useRef } from "react";
 
-function Splash() {
-    return<>
-    <div className={styles.container}>
-        <p id={styles.title}>environs</p>
-    </div>
+function Splash({ onAnimationEnd }) {
+    const titleRef = useRef(null);
+
+    useEffect(() => {
+        const titleElement = titleRef.current;
+
+        const handleAnimationEnd = () => {
+            if (onAnimationEnd) {
+                onAnimationEnd();
+            }
+        };
+
+        if (titleElement) {
+            titleElement.addEventListener('animationend', handleAnimationEnd);
+            return () => titleElement.removeEventListener('animationend', handleAnimationEnd);
+        }
+    }, [onAnimationEnd]);
+
+    return <>
+        <div className={styles.container}>
+            <p ref={titleRef} id={styles.title}>environs</p>
+        </div>
     </>;
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,16 @@
 
 import { useState } from "react";
 import Header from "./components/header";
-import Hero from "./components/hero";
 import Splash from "./components/splash";
 
 export default function Home() {
   const [showSplash, setShowSplash] = useState(true);
-  setTimeout(() => {
+
+  const handleSplashAnimationEnd = () => {
     setShowSplash(false);
-  }, 1200);
+  };
+
   return <>
-    {showSplash ? <Splash /> : <Header />};
+    {showSplash ? <Splash onAnimationEnd={handleSplashAnimationEnd} /> : <Header />};
   </>;
 }
